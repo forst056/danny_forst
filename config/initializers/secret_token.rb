21fbb002c7e62df6ea4608d73a4870a9fd7adcb9
Danny Forst <forst056@gmail.com>
2013-01-17 15:19:30 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DannyForst::Application.config.secret_token = 'bc2bc278d1160a1a403484aefc06679d91349b1ab5de416161d29169a170dec6a3ea667f742bca5fd78c7fa7ae5f9eb3fdb20a70aa3bbd8ed8c64cd4294c5401'
